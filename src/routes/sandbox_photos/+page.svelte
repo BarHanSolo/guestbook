f0b7af83e6c0f6dfb94728f414a34d7f8a183036
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2024-08-10 21:11:59 +0200

    some funny changes

--- src/routes/sandbox_photos/+page.svelte
+++ src/routes/sandbox_photos/+page.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
 	import Gallery from 'svelte-image-gallery';
-    
-    export let data: { photos: string[] } | undefined; // Typ danych z load
-    let imageNames: string[] = data?.photos || [];  // Używamy zdjęć z load
 
-    const imageUrls = imageNames.map(name => `${name}`);
+	export let data: { photos: string[] } | undefined; // Typ danych z load
+	let imageNames: string[] = data?.photos || []; // Używamy zdjęć z load
+
+	const imageUrls = imageNames.map((name) => `${name}`);
 
 	let selectedImage: string | null = null; // Przechowuje URL wybranego obrazu
-    let showModal = false;    // Kontroluje widoczność modala
+	let showModal = false; // Kontroluje widoczność modala
 
-     // Funkcja wywoływana po kliknięciu na miniaturkę
+	// Funkcja wywoływana po kliknięciu na miniaturkę
 	function openModal(url: string) {
-        console.log('Opening modal for image:', url);
-        selectedImage = url;
-        showModal = true;
-    }
+		console.log('Opening modal for image:', url);
+		selectedImage = url;
+		showModal = true;
+	}
 
-    // Funkcja zamykająca modal
-    function closeModal() {
-        console.log('Closing modal');
-        showModal = false;
-        selectedImage = null;
-    }
+	// Funkcja zamykająca modal
+	function closeModal() {
+		console.log('Closing modal');
+		showModal = false;
+		selectedImage = null;
+	}
 
 	// Funkcja obsługująca kliknięcia w galerii
 	function handleClick(e: CustomEvent) {
@@ -29,15 +29,15 @@
 		openModal(url); // Otwiera modal z wybranym obrazem
 	}
 
-    console.log('Image URLs:', imageUrls);
+	console.log('Image URLs:', imageUrls);
 </script>
 
 <h2 class="text-2xl">Photos</h2>
-<br>
+<br />
 <div class="flex flex-wrap gap-4">
 	<Gallery gap="10" maxColumnWidth="200" on:click={handleClick}>
 		{#each imageUrls as url}
-		<img src={url} alt="" style="cursor: pointer;">
+			<img src={url} alt="" style="cursor: pointer;" />
 		{/each}
 	</Gallery>
 </div>
@@ -46,50 +46,54 @@
 {#if showModal}
 	<div class="modal" role="dialog" tabindex="0" on:click={closeModal}>
 		<div class="modal-content" role="document" on:click|stopPropagation>
-			<button class="modal-close" aria-label="Close" on:click={closeModal}>
-				&times;
-			</button>
-			
-			<img src={selectedImage} alt="">
+			<button class="modal-close" aria-label="Close" on:click={closeModal}> &times; </button>
+
+			<img src={selectedImage} alt="" />
 		</div>
 	</div>
 {/if}
 
 <style>
-	:global(img) { opacity: .9; transition: all .2s }
-	:global(img):hover { opacity: 1; transform: scale(1.04) }
+	:global(img) {
+		opacity: 0.9;
+		transition: all 0.2s;
+	}
+	:global(img):hover {
+		opacity: 1;
+		transform: scale(1.04);
+	}
 
 	/* Stylowanie modala */
-    .modal {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.8);
-        z-index: 1000;
-    }
+	.modal {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.8);
+		z-index: 1000;
+	}
 
-    .modal-content {
-        position: relative;
-        max-width: 80%;
-        max-height: 80%;
-    }
+	.modal-content {
+		position: relative;
+		max-width: 80%;
+		max-height: 80%;
+	}
 
-    .modal-content img {
-        width: 100%;
-        height: auto;
-    }
+	.modal-content img {
+		width: 100%;
+		height: auto;
+	}
 
-    .close {
-        position: absolute;
-        top: 10px;
-        right: 20px;
-        font-size: 40px;
-        color: white;
-        cursor: pointer;
-    }
+	.close {
+		position: absolute;
+		top: 10px;
+		right: 20px;
+		font-size: 40px;
+		color: white;
+		cursor: pointer;
+	}
 </style>
