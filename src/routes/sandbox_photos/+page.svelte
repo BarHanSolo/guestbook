be888f02ad44e6483179868579692179d776e49f
BarHanSolo <andrzej.lippa@gmail.com>
2024-08-10 13:12:09 +0200

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	new file:   src/routes/sandbox_photos/+page.svelte added temp training module to show gallery

---
+++ src/routes/sandbox_photos/+page.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	import Gallery from 'svelte-image-gallery';
+	const imageNames = ['20240225_110247.jpg', 'piekanirk.png', 'signal-2024-07-26-171730.jpeg'];  //na razie fotki są zahardkdowane, napisać funkcję trzeba która załaduje je z bazy
+    const imageUrls = imageNames.map(name => `/uploads/${name}`);
+
+	let selectedImage: string | null = null; // Przechowuje URL wybranego obrazu
+    let showModal = false;    // Kontroluje widoczność modala
+
+     // Funkcja wywoływana po kliknięciu na miniaturkę
+	 function openModal(url: string) {
+        console.log('Opening modal for image:', url);
+        selectedImage = url;
+        showModal = true;
+    }
+
+    // Funkcja zamykająca modal
+    function closeModal() {
+        console.log('Closing modal');
+        showModal = false;
+        selectedImage = null;
+    }
+
+	// Funkcja obsługująca kliknięcia w galerii
+	function handleClick(e: CustomEvent) {
+		const url = e.detail.src; // Odczytuje URL obrazu z detali eventu
+		openModal(url); // Otwiera modal z wybranym obrazem
+	}
+
+    console.log('Image URLs:', imageUrls);
+</script>
+
+<h2 class="text-2xl">Photos</h2>
+<br>
+<div class="flex flex-wrap gap-4">
+	<Gallery gap="10" maxColumnWidth="200" on:click={handleClick}>
+		{#each imageUrls as url}
+		<img src={url} alt="" style="cursor: pointer;">
+		{/each}
+	</Gallery>
+</div>
+
+<!-- Modal -->
+{#if showModal}
+	<div class="modal" role="dialog" tabindex="0" on:click={closeModal}>
+		<div class="modal-content" role="document" on:click|stopPropagation>
+			<button class="modal-close" aria-label="Close" on:click={closeModal}>
+				&times;
+			</button>
+			
+			<img src={selectedImage} alt="">
+		</div>
+	</div>
+{/if}
+
+<style>
+	:global(img) { opacity: .9; transition: all .2s }
+	:global(img):hover { opacity: 1; transform: scale(1.04) }
+
+	/* Stylowanie modala */
+    .modal {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.8);
+        z-index: 1000;
+    }
+
+    .modal-content {
+        position: relative;
+        max-width: 80%;
+        max-height: 80%;
+    }
+
+    .modal-content img {
+        width: 100%;
+        height: auto;
+    }
+
+    .close {
+        position: absolute;
+        top: 10px;
+        right: 20px;
+        font-size: 40px;
+        color: white;
+        cursor: pointer;
+    }
+</style>
