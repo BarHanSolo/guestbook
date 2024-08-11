8c81bd69984d8a94b09ab639554421da2a6155ab
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2024-08-11 23:01:07 +0200

    generating jwt

---
+++ src/lib/components/login-dialog/LoginDialog.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { getBrowserToken } from '$lib/utils/browser-token';
+	import { onMount } from 'svelte';
+
+	let dialog: HTMLDialogElement;
+	let token: string | null;
+
+	onMount(() => {
+		token = getBrowserToken();
+		// if (!token) {
+		// 	dialog.showModal();
+		// }
+	});
+</script>
+
+<dialog bind:this={dialog}>hello</dialog>
