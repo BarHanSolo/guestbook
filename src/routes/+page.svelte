716d746697b28c0d8a21a44b3c78bd8ee29c093d
Andrzej Lippa <andrzej.lippa@comarch.com>
2024-08-16 22:40:07 +0200

    added ramka with życzenia

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<section class="flex flex-col gap-4">
+	<section class="flex flex-col items-center gap-6">
+		<p
+			class="bg-white bg-opacity-50 backdrop-blur-md text-lg text-center p-4 rounded-md shadow-md text-millbrook-900"
+		>
+			Podziel się swoim własnym spojrzeniem na wyjątkowy dzień w życiu <b>Anny</b> i <b>Jakuba</b> –
+			dodaj do Księgi Gości swoje kadry ze świętowania ich zaślubin.
+		</p>
+	</section>
+</section>
