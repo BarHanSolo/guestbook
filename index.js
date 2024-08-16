f5f27991602ca2fd489323246e77b1415ede3c9e
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2024-08-16 21:53:04 +0200

    moved before handler

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import { handler } from './build/handler.js';
 
 const app = express();
 app.use(cors());
-app.use(handler);
 app.get('/uploads/*', express.static('.'));
+app.use(handler);
 
 app.listen(4000, () => {
 	console.log('listening on port 40000');
