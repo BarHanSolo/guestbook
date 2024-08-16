a6d7704793a7768fd4cf820890d7f0eba0ff8bdd
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2024-08-16 22:00:50 +0200

    delete file

--- src/routes/gallery-user/+server.ts
+++ src/routes/gallery-user/+server.ts
@@ -1,23 +1,24 @@
 import { json } from '@sveltejs/kit';
 import fs from 'fs/promises'; // Upewnij się, że korzystasz z odpowiedniej wersji Node.js, która obsługuje fs/promises
+import path from 'path';
 
 export async function DELETE({ request }: { request: Request }) {
-    const { filename } = await request.json();
-    console.log(filename)
+	const { filename } = await request.json();
+	console.log(filename);
 
-    if (!filename) {
-        return json({ error: 'Filename is missing' }, { status: 400 });
-    }
+	if (!filename) {
+		return json({ error: 'Filename is missing' }, { status: 400 });
+	}
 
-    try {
-        // Zakładam, że pliki są przechowywane w folderze '/uploads/photos'
-        const filePath = `${filename}`;
-        await fs.unlink(filePath); // Usuwa plik z serwera
-        const thumbnailPath = filePath.replace('/photos/', '/thumbnails/');
-        await fs.unlink(thumbnailPath);
-        return json({ message: 'File deleted successfully' });
-    } catch (error) {
-        console.error(`Error deleting file: ${error}`);
-        return json({ error: 'File deletion failed' }, { status: 500 });
-    }
+	try {
+		// Zakładam, że pliki są przechowywane w folderze '/uploads/photos'
+		const photosDirPath = path.resolve(filename);
+		const thumbnailsDirPath = path.resolve(filename.replace('/photos/', '/thumbnails/'));
+		await fs.unlink(photosDirPath); // Usuwa plik z serwera
+		await fs.unlink(thumbnailsDirPath);
+		return json({ message: 'File deleted successfully' });
+	} catch (error) {
+		console.error(`Error deleting file: ${error}`);
+		return json({ error: 'File deletion failed' }, { status: 500 });
+	}
 }
